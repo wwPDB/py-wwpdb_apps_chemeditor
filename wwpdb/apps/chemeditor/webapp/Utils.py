9a3bc486646f8412d311ae35dd2287398bd82227
Weslley Morellato <wbueno@ebi.ac.uk>
2021-07-15 08:46:54 -0400

    Preventing setupLog from adding multiple handlers to the process logger instance

--- wwpdb/apps/chemeditor/webapp/Utils.py
+++ wwpdb/apps/chemeditor/webapp/Utils.py
@@ -1,5 +1,7 @@
 from logging import getLogger, StreamHandler, Formatter, DEBUG, INFO
 
+HANDLER_NAME = 'stream_handler'
+
 def setupLog(verbose, log_file):
     """Setup a Logger instance to use the same file as provided
     by the 'log' parameters
@@ -11,7 +13,15 @@ def setupLog(verbose, log_file):
         Logger: instance of Logger class
     """
     logger = getLogger(__name__)
+
+    if logger.hasHandlers():
+        for h in logger.handlers:
+            # this field is not documented
+            if h.get_name() == HANDLER_NAME:
+                return logger
+    
     handler = StreamHandler(log_file)
+    handler.set_name(HANDLER_NAME)
 
     formatter = Formatter('+%(module)s.%(funcName)s() ++ %(message)s\n')
     handler.setFormatter(formatter)
