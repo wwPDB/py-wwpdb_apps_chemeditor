06afe1c9841610689376630fb49ef44bf0dca700
Ezra Peisach <ezra.peisach@rcsb.org>
2023-04-01 20:17:26 -0400

    Test retrieving CCD codes from list

--- wwpdb/apps/tests-chemeditor/ChemEditorWebAppTests.py
+++ wwpdb/apps/tests-chemeditor/ChemEditorWebAppTests.py
@@ -5,10 +5,10 @@ import unittest
 import os
 import os.path
 from unittest.mock import Mock, MagicMock, patch
-from wwpdb.apps.chemeditor.webapp.ChemEditorWebApp import ChemEditorWebAppWorker
-from wwpdb.utils.session.WebRequest import InputRequest
 
 sessionsTopDir = tempfile.mkdtemp()
+refdir = tempfile.TemporaryDirectory()
+
 configInfo = {
     'SITE_DEPOSIT_STORAGE_PATH': tempfile.mkdtemp(),
     'SITE_PREFIX': 'PDBE_LOCALHOST',
@@ -17,6 +17,7 @@ configInfo = {
     'SITE_CC_APPS_PATH': tempfile.mkdtemp(),
     'SITE_CC_CVS_PATH': tempfile.mkdtemp(),
     'SITE_DB_PORT_NUMBER': 10,
+    'REFERENCE_PATH': refdir.name,
 }
 
 configInfoMockConfig = {
@@ -26,6 +27,10 @@ configInfoMockConfig = {
 configMock = MagicMock(**configInfoMockConfig)
 
 sys.modules['wwpdb.utils.config.ConfigInfo'] = Mock(ConfigInfo=configMock)
+
+# These must be after the definitions - before wwpdb.utils.config imported anywhere
+from wwpdb.apps.chemeditor.webapp.ChemEditorWebApp import ChemEditorWebAppWorker  # noqa: E402
+from wwpdb.utils.session.WebRequest import InputRequest  # noqa: E402
 
 
 class ChemEditorWebAppTests(unittest.TestCase):
@@ -60,3 +65,22 @@ class ChemEditorWebAppTests(unittest.TestCase):
         self._reqObj.setValue("request_path", "/service/chemeditor/get_entries_with_ligand")
         response = json.loads(cewa.doOp().get()['RETURN_STRING'])
         self.assertEqual(response['errortext'], "Could not open a connection to the database")
+
+    @patch('wwpdb.apps.chemeditor.webapp.ChemEditorWebApp.ChemEditorBase', autospec=True)
+    def testGetNextAccession(self, mockcvs):
+        """Tests retrieval of the next CCD code"""
+
+        mockcvs().getSandBoxFilePath.return_value = False
+
+        iddir = os.path.join(configInfo["REFERENCE_PATH"], "id_codes")
+        if not os.path.isdir(iddir):
+            os.mkdir(iddir)
+        with open(os.path.join(iddir, "unusedCodes.lst"), "w") as fout:
+            fout.write("1AB\nCDEFG\n")
+
+        cewa = ChemEditorWebAppWorker(self._reqObj, self._verbose, self._lfh)
+        self._reqObj.setValue("request_path", "/service/chemeditor/get_new_code")
+        response = json.loads(cewa.doOp().get()['RETURN_STRING'])
+        self.assertEqual(response['textcontent'], '1AB')
+        response = json.loads(cewa.doOp().get()['RETURN_STRING'])
+        self.assertEqual(response['textcontent'], 'CDEFG')
